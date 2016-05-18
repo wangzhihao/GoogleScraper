82a08de6ea1b0147e8165358d227ce0477d0ad65
zhihao wang <hao@opentown.cn>
2016-05-19 02:12:03 +0800

    use real composer data

--- get_composition.py
+++ get_composition.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from GoogleScraper import scrape_with_config, GoogleSearchError
+import json
+
+with open('composers.json') as data_file:    
+    data = json.load(data_file)
+
+keywords = map((lambda item: item['name'] + ' compositions') ,filter(lambda x: 'name' in x, data))
 
 # See in the config.cfg file for possible values
 config = {
-    'keyword': 'chopin compositions',
+    'keywords': keywords,
     'search_engines': ['google'],
     'num_pages_for_keyword': 1,
     'scrape_method': 'http',
     'log_level': 'NOTSET',
-    'do_caching': 'True',
-    'output_filename': 'compositions.json'
+    'do_caching': 'True'
 }
 
 try:
     search = scrape_with_config(config)
 except GoogleSearchError as e:
     print(e)
-
-for serp in search.serps:
-    print(serp)
-    for link in serp.links:
-        print(link)
