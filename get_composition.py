f9a31d4a304298bcae021e7f23cdb4fca4393d3a
zhihao wang <hao@opentown.cn>
2016-05-19 09:58:39 +0800

    add proxy

--- get_composition.py
+++ get_composition.py
@@ -16,6 +16,8 @@ config = {
         1:  (2, 3)
     },
     'num_pages_for_keyword': 1,
+    'num_workers' : 2,
+    'proxy_file' : 'proxies.txt',
     'scrape_method': 'http',
     'log_level': 'NOTSET',
     'do_caching': 'True',
